dd8185014f6bc3cfbdcedc5a1cc7e62e0daa7751
Le Vivilet <levivlet@pm.me>
2025-06-22 21:30:25 +0200

    tsc

--- packages/clipboard-worker/src/parts/WriteText/WriteText.ts
+++ packages/clipboard-worker/src/parts/WriteText/WriteText.ts
@@ -5,6 +5,7 @@ import { VError } from '../VError/VError.ts'
 export const writeText = async (text: string): Promise<void> => {
   try {
     Assert.string(text)
+    // @ts-ignore
     await RendererProcess.invoke('ClipBoard.writeText', /* text */ text)
   } catch (error) {
     throw new VError(error, 'Failed to write text to clipboard')
