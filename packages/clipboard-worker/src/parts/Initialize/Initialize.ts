28efd057758e2cd1c5b2179eae1b0d6578729d61
Le Vivilet <levivlet@pm.me>
2025-06-23 13:28:28 +0200

    feature: add initializeSharedProcess

--- packages/clipboard-worker/src/parts/Initialize/Initialize.ts
+++ packages/clipboard-worker/src/parts/Initialize/Initialize.ts
@@ -1,6 +1,9 @@
 import { initializeRendererProcess } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
+import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
 
 export const initialize = async (platform: number): Promise<void> => {
   // TODO if platform is remote or electron, create shared process rpc
   await initializeRendererProcess()
+
+  await initializeSharedProcess()
 }
---
+++ packages/clipboard-worker/src/parts/InitializeSharedProcess/InitializeSharedProcess.ts
@@ -0,0 +1,1 @@
+export const initializeSharedProcess = async (): Promise<void> => {}
