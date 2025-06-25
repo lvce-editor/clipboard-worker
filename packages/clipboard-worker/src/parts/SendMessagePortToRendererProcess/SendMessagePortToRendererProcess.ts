4244ebc5871490bdd798c49cce49989af63b1a20
Le Vivilet <levivlet@pm.me>
2025-06-25 15:17:30 +0200

    fix: sending port to renderer process

--- packages/clipboard-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
+++ packages/clipboard-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
@@ -2,11 +2,7 @@ import { RpcId } from '@lvce-editor/rpc-registry'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToRendererProcess = async (port: MessagePort): Promise<void> => {
-  const command = 'HandleMessagePort.handleMessagePort2'
-  await RendererWorker.invokeAndTransfer(
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
-    port,
-    command,
-    RpcId.DebugWorker,
-  )
+  const command = 'HandleMessagePort.handleMessagePort'
+  // @ts-ignore
+  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToRendererProcess', port, command, RpcId.DebugWorker)
 }
