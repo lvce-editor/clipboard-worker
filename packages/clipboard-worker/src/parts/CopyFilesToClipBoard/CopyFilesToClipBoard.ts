17484eb7a2cc8e4f1e0bb8f1ba0ac4634af83482
Le Vivilet <levivlet@pm.me>
2025-06-26 13:47:28 +0200

    feature: add copyFilesToClipBoardWeb

--- packages/clipboard-worker/src/parts/CopyFilesToClipBoard/CopyFilesToClipBoard.ts
+++ packages/clipboard-worker/src/parts/CopyFilesToClipBoard/CopyFilesToClipBoard.ts
@@ -1,11 +1,13 @@
 import { copyFilesToClipBoardGnome } from '../CopyFilesToClipBoardGnome/CopyFilesToClipBoardGnome.ts'
+import { copyFilesToClipBoardWeb } from '../CopyFilesToClipBoardWeb/CopyFilesToClipBoardWeb.ts'
 
 const isGnome = (): boolean => {
-  return true
+  return false
 }
 
 export const copyFilesToClipBoard = async (type: string, files: string[]): Promise<void> => {
   if (isGnome()) {
     return copyFilesToClipBoardGnome(type, files)
   }
+  await copyFilesToClipBoardWeb(files)
 }
