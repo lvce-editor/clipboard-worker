935400b5f5fed62be6d60d43949c50c4206ac14c
Le Vivilet <levivlet@pm.me>
2025-06-28 15:11:01 +0200

    feature: disable clipboard process

--- packages/clipboard-worker/src/parts/Initialize/Initialize.ts
+++ packages/clipboard-worker/src/parts/Initialize/Initialize.ts
@@ -1,10 +1,8 @@
 import { initializeRendererProcess } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
-import { initializeSharedProcess } from '../InitializeSharedProcess/InitializeSharedProcess.ts'
 import * as PlatformState from '../PlatformState/PlatformState.ts'
 
 export const initialize = async (platform: number): Promise<void> => {
   PlatformState.set(platform)
   // TODO if platform is remote or electron, create shared process rpc
   await initializeRendererProcess()
-  await initializeSharedProcess(platform)
 }
