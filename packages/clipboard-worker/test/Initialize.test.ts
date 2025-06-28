bc212d5af3ed6d41ab30c41e742579b7e2491d7a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-28 15:13:31 +0200

    feature: disable clipboard process (#38)
    * feature: disable clipboard process
    
    * tsc
    
    * tsc

--- packages/clipboard-worker/test/Initialize.test.ts
+++ packages/clipboard-worker/test/Initialize.test.ts
@@ -1,17 +1,10 @@
 import { test, expect, jest, beforeEach } from '@jest/globals'
 
 const mockInitializeRendererProcess = jest.fn() as jest.MockedFunction<any>
-const mockInitializeSharedProcess = jest.fn() as jest.MockedFunction<any>
 
 jest.unstable_mockModule('../src/parts/InitializeRendererProcess/InitializeRendererProcess.ts', () => {
   return {
     initializeRendererProcess: mockInitializeRendererProcess,
-  }
-})
-
-jest.unstable_mockModule('../src/parts/InitializeSharedProcess/InitializeSharedProcess.ts', () => {
-  return {
-    initializeSharedProcess: mockInitializeSharedProcess,
   }
 })
 
@@ -25,5 +18,4 @@ test('initialize should call both initialization functions', async () => {
   await Initialize.initialize(0)
 
   expect(mockInitializeRendererProcess).toHaveBeenCalled()
-  expect(mockInitializeSharedProcess).toHaveBeenCalled()
 })
