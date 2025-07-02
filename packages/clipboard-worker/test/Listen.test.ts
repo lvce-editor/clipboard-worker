b3c8dc6f0b27e80d0934e178cc4d3bb99bcdf227
Le Vivilet <levivlet@pm.me>
2025-07-02 16:55:19 +0200

    feature: simplify main test

--- packages/clipboard-worker/test/Listen.test.ts
+++ packages/clipboard-worker/test/Listen.test.ts
@@ -1,49 +1,11 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-jest.unstable_mockModule('@lvce-editor/rpc', () => {
-  return {
-    WebWorkerRpcClient: {
-      create: jest.fn(() => {
-        return {
-          dispose: jest.fn(),
-        }
-      }),
-    },
-    PlainMessagePortRpcParent: {
-      create: jest.fn(() => {
-        return {
-          dispose: jest.fn(),
-        }
-      }),
-    },
-    MessagePortRpcParent: {
-      create: jest.fn(() => {
-        return {
-          dispose: jest.fn(),
-        }
-      }),
-    },
-    WebSocketRpcParent: {
-      create: jest.fn(() => {
-        return {
-          dispose: jest.fn(),
-        }
-      }),
-    },
-  }
-})
-
-const Listen = await import('../src/parts/Listen/Listen.ts')
-const rpc = await import('@lvce-editor/rpc')
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
+import { test } from '@jest/globals'
+import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
+import * as Listen from '../src/parts/Listen/Listen.ts'
 
 test('listen', async () => {
-  await Listen.listen()
-  expect(rpc.WebWorkerRpcClient.create).toHaveBeenCalledTimes(1)
-  expect(rpc.WebWorkerRpcClient.create).toHaveBeenCalledWith({
-    commandMap: expect.any(Object),
-  })
+  const { start, dispose } = mockWorkerGlobalRpc()
+  const listenPromise = Listen.listen()
+  start()
+  await listenPromise
+  dispose()
 })
