76376f584f005dcf76487733efeea5540bc02062
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-27 17:22:02 +0200

    feature: use memory clipboard (#63)

--- packages/clipboard-worker/src/parts/WriteText/WriteText.ts
+++ packages/clipboard-worker/src/parts/WriteText/WriteText.ts
@@ -1,10 +1,15 @@
 import * as Assert from '../Assert/Assert.ts'
+import * as MemoryClipBoardState from '../MemoryClipBoardState/MemoryClipBoardState.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import { VError } from '../VError/VError.ts'
 
 export const writeText = async (text: string): Promise<void> => {
   try {
     Assert.string(text)
+    if (MemoryClipBoardState.get()) {
+      MemoryClipBoardState.writeText(text)
+      return
+    }
     // @ts-ignore
     await RendererProcess.invoke('ClipBoard.writeText', /* text */ text)
   } catch (error) {
