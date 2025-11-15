9eff5898f0486bf3ff715b802036e42b91760687
Le Vivilet <levivilet@pm.me>
2025-11-15 20:14:10 +0100

    feature: allow using memory clipboard for readText (#78)
    * feature: allow using memory clipboard for readText
    
    * fix
    
    * lint

--- packages/clipboard-worker/src/parts/ReadText/ReadText.ts
+++ packages/clipboard-worker/src/parts/ReadText/ReadText.ts
@@ -1,10 +1,14 @@
 import { isClipboardUnsupportedError } from '../IsClipboardUnsupportedError/IsClipboardUnsupportedError.ts'
 import { isPermissionDeniedError } from '../IsPermissionDeniedError/IsPermissionDeniedError.ts'
+import * as MemoryClipBoardState from '../MemoryClipBoardState/MemoryClipBoardState.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import { VError } from '../VError/VError.ts'
 
-export const readText = async (): Promise<any> => {
+export const readText = async (): Promise<string> => {
   try {
+    if (MemoryClipBoardState.get()) {
+      return MemoryClipBoardState.readText()
+    }
     // @ts-ignore
     return await RendererProcess.invoke('ClipBoard.readText')
   } catch (error) {
