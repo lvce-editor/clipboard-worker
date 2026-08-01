ff50f881c82c7a47576d3e49f20bf53f61278d72
Le Vivilet <levivilet@pm.me>
2026-08-01 02:00:15 -0700

    feature: request websocket capabilities (#125)

--- packages/clipboard-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
+++ packages/clipboard-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
@@ -1,14 +1,29 @@
 import { type Rpc, WebSocketRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
 import * as GetWebSocketUrl from '../GetWebSocketUrl/GetWebSocketUrl.ts'
 import * as Location from '../Location/Location.ts'
 
-export const createClipBoardProcessRpcNode = async (): Promise<Rpc> => {
+const createWebSocket = async (): Promise<WebSocket> => {
   try {
+    const { protocols, url } = (await RendererWorker.invoke('WebSocketCapability.create', 'clipboard-process')) as {
+      readonly protocols: string[]
+      readonly url: string
+    }
+    return new WebSocket(url, protocols)
+  } catch (error) {
+    if (!(error instanceof Error && error.message.includes('WebSocketCapability.create') && /command not found|not found/i.test(error.message))) {
+      throw error
+    }
     const host = Location.getHost()
     const protocol = Location.getProtocol()
-    const wsUrl = GetWebSocketUrl.getWebSocketUrl('clipboard-process', host, protocol)
-    const webSocket = new WebSocket(wsUrl)
+    return new WebSocket(GetWebSocketUrl.getWebSocketUrl('clipboard-process', host, protocol))
+  }
+}
+
+export const createClipBoardProcessRpcNode = async (): Promise<Rpc> => {
+  try {
+    const webSocket = await createWebSocket()
     const rpc = await WebSocketRpcParent.create({
       commandMap: {},
       webSocket,
