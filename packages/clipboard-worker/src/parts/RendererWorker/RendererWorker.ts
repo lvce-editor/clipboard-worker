34dd0f08c0a66582d1a8519ac7bef17727790559
Le Vivilet <levivilet@pm.me>
2026-08-01 22:42:07 +0000

    feature: update eslint dependencies

--- packages/clipboard-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/clipboard-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,11 +1,25 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const {
-  dispose,
-  invoke,
-  invokeAndTransfer,
-  sendMessagePortToEditorWorker,
-  sendMessagePortToExtensionHostWorker,
-  sendMessagePortToRendererProcess,
-  set,
-} = RendererWorker
+export const dispose = (...args: Readonly<Parameters<typeof RendererWorker.dispose>>): ReturnType<typeof RendererWorker.dispose> =>
+  RendererWorker.dispose(...args)
+
+export const invoke = (...args: Readonly<Parameters<typeof RendererWorker.invoke>>): ReturnType<typeof RendererWorker.invoke> =>
+  RendererWorker.invoke(...args)
+
+export const invokeAndTransfer = (
+  ...args: Readonly<Parameters<typeof RendererWorker.invokeAndTransfer>>
+): ReturnType<typeof RendererWorker.invokeAndTransfer> => RendererWorker.invokeAndTransfer(...args)
+
+export const sendMessagePortToEditorWorker = (
+  ...args: Readonly<Parameters<typeof RendererWorker.sendMessagePortToEditorWorker>>
+): ReturnType<typeof RendererWorker.sendMessagePortToEditorWorker> => RendererWorker.sendMessagePortToEditorWorker(...args)
+
+export const sendMessagePortToExtensionHostWorker = (
+  ...args: Readonly<Parameters<typeof RendererWorker.sendMessagePortToExtensionHostWorker>>
+): ReturnType<typeof RendererWorker.sendMessagePortToExtensionHostWorker> => RendererWorker.sendMessagePortToExtensionHostWorker(...args)
+
+export const sendMessagePortToRendererProcess = (
+  ...args: Readonly<Parameters<typeof RendererWorker.sendMessagePortToRendererProcess>>
+): ReturnType<typeof RendererWorker.sendMessagePortToRendererProcess> => RendererWorker.sendMessagePortToRendererProcess(...args)
+
+export const set = (...args: Readonly<Parameters<typeof RendererWorker.set>>): ReturnType<typeof RendererWorker.set> => RendererWorker.set(...args)
