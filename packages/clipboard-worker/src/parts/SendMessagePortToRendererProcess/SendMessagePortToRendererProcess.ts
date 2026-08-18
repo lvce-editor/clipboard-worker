6ac8b6ba0efcd84f8b669e78e5c360b4fe0c9516
Le Vivilet <levivilet@pm.me>
2026-08-18 11:08:04 -0700

    fix: use clipboard worker rpc id (#133)

--- packages/clipboard-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
+++ packages/clipboard-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ts
@@ -4,5 +4,5 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 export const sendMessagePortToRendererProcess = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
   // @ts-ignore
-  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToRendererProcess', port, command, RpcId.DebugWorker)
+  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToRendererProcess', port, command, RpcId.ClipBoardWorker)
 }
